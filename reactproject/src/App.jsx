0d8d11423c41113db60dbc78fb1a676a3979ae68
StivenMolinaG <stmolinag@gmail.com>
2023-03-01 21:03:11 -0500

    forms

--- reactproject/src/App.jsx
+++ reactproject/src/App.jsx
@@ -1,23 +1,40 @@
 import './App.css'
-import TextName from './components/TextName/TextName'
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { useState } from 'react';
 
 function App() {
+  const [name, setName] = useState("Stiven");
+  const [edad, setEdad] = useState(0);
+  const [email, setEmail] = useState("email@gmail.com");
 
-  const Padre = (props) => {
-    return (
-    <div style={{backgroundColor: "white", width: "300px", height: "150px"}}>
-      <h1 style={{color: "black"}}> Hola </h1>
-      {props.children}
-    </div>
-    )
+  const handleNameChange = (event) => {
+    setName(event.target.value)
+  }
+  const handleEdadChange = (event) => {
+    setEdad(event.target.value)
+  }
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value)
   }
 
+  const handleClick = () => {
+    console.log("Datos: " );
+    console.log("Name: ", name)
+    console.log("Edad: ", edad)
+    console.log("Email: ", email)
+  }
   return (
     <div className="App">
-      <Padre>
-        <TextName name='Stiven Molina' />
-        <TextName name='Catalina Restrepo' />
-      </Padre>
+      <div className='divHola'>
+        <h1 className='title'>Esto es un formulario</h1>
+        <div className='divInputs'>
+        <TextField id="outlined-basic" label="Nombre" variant="outlined" onChange={handleNameChange}/>
+        <TextField id="outlined-basic" label="Edad" variant="outlined" type='number' onChange={handleEdadChange}/>
+        <TextField id="outlined-basic" label="Email" variant="outlined" type='email' onChange={handleEmailChange}/>
+        <Button variant="contained" onClick={handleClick}>Enviar</Button>
+        </div>
+      </div>
     </div>
   )
 }
